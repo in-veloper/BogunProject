35f1d99eb6d0c0e906c62e413ed21c9a2fb62009
Yeongin Jeong <yijung@nsetec.com>
2022-12-29 15:55:40 +0900

    12.29 학교 정보 조회 API 처리 중 commit

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,41 +1,21 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable array-callback-return */
+/* eslint-disable react-hooks/rules-of-hooks */
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
- 
+// import { setegid } from 'process';
+
+const URL = "https://open.neis.go.kr/hub/schoolInfo";
+
 const Register = () => {
-    // const [data, setData] = useState(null);
-    // const [loading, setLoading] = useState(false);
-    // const [error, setError] = useState(null);
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
 
-    // const fetchData = async () => {
-    //     try {
-    //         setError(null);
-    //         setData(null);
-    //         setLoading(true);
-
-    //         const response = await axios.get("http://api.data.go.kr/openapi/tn_pubr_public_elesch_mskul_lc_api", {
-    //             params: {
-    //                 serviceKey : process.env.REACT_APP_API_KEY,
-    //                 stdt : 2022
-    //             }
-    //         });
-
-    //         setData(response.data);
-    //     } catch (e) {
-    //         setError(e);
-    //     }
-    //     setLoading(false);
-    // };
-
-    // useEffect(() => {
-    //     fetchData();
-    // }, []);
-
-    // if(loading) return <div>Loading</div>;
-    // if(error) return <div>Error</div>;
-    // if(!data) return null;
+    const [schoolName, setSchoolName] = useState('');
 
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
@@ -43,7 +23,58 @@ const Register = () => {
     const [confPassword, setConfPassword] = useState('');
     const [msg, setMsg] = useState('');
     const navigate = useNavigate();
- 
+    
+    const getSchoolInfo = (e) => {
+        try {
+            setLoading(true);
+            axios.get(URL, {
+                params: {
+                    KEY: "1addcd8b3de24aa5920d79df1bbe2ece",
+                    pIndex: 1,
+                    pSize: 100,
+                    Type: "json",
+                    SCHUL_NM: e
+                }
+            }).then((response) => {
+                const data = response.data;
+                
+                // data.schoolInfo가 없으면 값이 없을 떄
+                if(!data.schoolInfo) {
+                    setData(null)
+                } else {
+                    if(data.schoolInfo[1].row.length > 50) {
+                        setData(null);
+                    }else{
+                        setData(data.schoolInfo[1].row);
+                        // SchoolList(data.schoolInfo[1].row);
+                    }
+                }    
+                
+                // setData(data);
+                console.log(data);
+            });
+        } catch (error) {
+            console.log(error);
+            setError(error)
+        }
+        setLoading(false);
+    }
+
+    const searchHandle = (e) => {
+        setSchoolName(e.target.value);
+        getSchoolInfo(e.target.value);
+        // SchoolList(data);
+    }
+
+
+    useEffect(() => {
+        // fetchData();
+    }, [data]);
+
+    // if(loading) return <div>loading...</div>;
+    // if(error) return <div>Error...</div>;
+    // if(!data) return null;
+
     const Register = async (e) => {
         e.preventDefault();
         try {
@@ -61,6 +92,20 @@ const Register = () => {
         }
     }
  
+    // const SchoolList = (data) => {
+    //     if(data.length) {
+    //         debugger
+    //         return (
+    //                         data.forEach(item => {
+    //                             <li>item.SCHUL_NM</li>
+    //                         })
+    //                         // data.map((v, inx) => {
+    //                         //     <li>v.SCHUL_NM</li>
+    //                         // })
+    //         )
+    //     }
+    // }
+
     return (
         <section className="hero has-background-grey-light is-fullheight is-fullwidth">
             <div className="hero-body">
@@ -70,11 +115,8 @@ const Register = () => {
                             <form onSubmit={Register} className="box">
                                 <p className="has-text-centered">{msg}</p>
                                 <label className='label'>학교 선택</label>
-                                <div className='select is-fullwidth'>
-                                    <select>
-                                        <option>학교 선택</option>
-                                        <option>With options</option>
-                                    </select>
+                                <input className="input" name="searchSchool" type="text" value={schoolName} onChange={ searchHandle } placeholder="학교명을 입력하세요" />
+                                <div>
                                 </div>
                                 <div className="field mt-5">
                                     <label className="label">Name</label>
@@ -105,6 +147,9 @@ const Register = () => {
                                     <button className="button is-success is-fullwidth">Register</button>
                                 </div>
                             </form>
+                            {/* <div>
+                                <p>{ data.response }</p>
+                            </div> */}
                         </div>
                     </div>
                 </div>
