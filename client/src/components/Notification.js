9ee2a34e57167e178343157a896520fd5324da0f
Yeongin Jeong <yijung@nsetec.com>
2023-01-09 16:17:03 +0900

    01.09 Notification Style 추가 수정 commit

--- client/src/components/Notification.js
+++ client/src/components/Notification.js
@@ -4,7 +4,7 @@ const Notification = () => {
     return (
         <div className="notification is-warning" style={{ marginTop : 30, marginLeft : 277, marginRight : 277 }}>
             <button class="delete"></button>
-            <BiInfoSquare/>
+            <BiInfoSquare style={{ fontSize : 20, verticalAlign : 'middle', marginTop : -5, marginRight : 10 }}/>
             금일 등록된 보건일정이 없습니다.
         </div>
     )
