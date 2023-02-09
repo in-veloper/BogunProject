436f9a2a63bf65248f5f0c9a8993a7354ad11c1f
Yeongin Jeong <yijung@nsetec.com>
2023-02-09 16:02:54 +0900

    2.9 의약품 정보 Search View 처리 및 동작 처리 중 commit

--- client/src/components/MedicalInfo.js
+++ client/src/components/MedicalInfo.js
@@ -1,11 +1,14 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react-hooks/rules-of-hooks */
 import React, { useState, useEffect } from 'react';
 import axios from "axios";
-
-
+import { FcSearch } from 'react-icons/fc';
 
 const MedicalInfo = () => {
     const [data, setData] = useState(null);
+    const [searchCategory, setSearchCategory] = useState("");
+    const [toSearch, setToSearch] = useState(null);
+
     const URL = '/1471000/DrbEasyDrugInfoService/getDrbEasyDrugList';
     const fetchData = async(request) => {
         try {
@@ -43,7 +46,8 @@ const MedicalInfo = () => {
 
 
     if(data) {
-        debugger
+        // debugger
+        // 이제 값 잘 들어옴 data 안에 body에 있음
     }
     // const getEMedicalInfo = async() => {
     //     const URL = '/1471000/DrbEasyDrugInfoService';
@@ -68,9 +72,53 @@ const MedicalInfo = () => {
     //     setData(response.data);
     // }
 
+    const handleSearchCategory = (event) => {
+        const selectedCategory = event.target.value;
+        setSearchCategory(selectedCategory);
+        // setSearchCategory({
+        //     ...searchCategory,
+        //     searchCategory: selectedCategory
+        // });
+        // debugger
+    }
+
+    const handleSearch = (event) => {
+        
+        const toSearchText = document.getElementById('searchText').value;
+        const mediCategory = document.getElementById('mediCategory');
+        const selectedCategory = mediCategory.getElementsByTagName('select')[0].value;
+
+        setToSearch({
+            selectedCategory : selectedCategory,
+            toSearchText : toSearchText 
+        });
+        debugger
+    }
+
     return (
         <div className="container mt-5" style={{display: "flex", flexDirection: 'column', height: '100vh'}}>
             {/* <p>{data.response.body}</p> */}
+            <div id='searchBar'>
+                <div className="select is-small" id='mediCategory' style={{ float : 'left' }}>
+                    <select onChange={handleSearchCategory} value={searchCategory}>
+                        <option value='all'>전체</option>
+                        <option value='itemName'>제품명</option>
+                        <option value='companyName'>업체명</option>
+                        <option value='effect'>효능</option>
+                        <option value='itemCode'>품목기준코드</option>
+                    </select>
+                </div>
+                <div style={{ float : 'left', marginLeft : 20 }}>
+                    <input 
+                        className='input is-info is-small'
+                        placeholder='검색어를 입력하세요'
+                        id='searchText'
+                        style={{ width: 300 }}
+                    />
+                </div>
+                <FcSearch style={{ fontSize : 20, marginBottom : -6, marginLeft : 5 }} onClick={handleSearch}/>
+            </div>
+            
         </div>
     )
 }
